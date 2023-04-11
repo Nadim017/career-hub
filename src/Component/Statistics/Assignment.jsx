141e763c404fcb054437bb48e7ea524b07a68a37
nadim <mdnadimmahamud704@gmail.com>
2023-04-11 23:02:28 +0600

    pie chart updated with adding legend

--- src/Component/Statistics/Assignment.jsx
+++ src/Component/Statistics/Assignment.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Legend, Tooltip } from 'recharts';
 
 const Assignment = () => {
   const assignmentMarks = [
-    { assignment: 1, marks: 60 },
-    { assignment: 2, marks: 59 },
-    { assignment: 3, marks: 60 },
-    { assignment: 4, marks: 60 },
-    { assignment: 5, marks: 60 },
-    { assignment: 6, marks: 60 },
-    { assignment: 7, marks: 60 },
-    { assignment: 8, marks: 60 },
+    { assignment: 'assigment-1', marks: 60 },
+    { assignment: 'assigment-2', marks: 59 },
+    { assignment: 'assigment-3', marks: 60 },
+    { assignment: 'assigment-4', marks: 60 },
+    { assignment: 'assigment-5', marks: 60 },
+    { assignment: 'assigment-6', marks: 60 },
+    { assignment: 'assigment-7', marks: 60 },
+    { assignment: 'assigment-8', marks: 60 },
   ];
   return (
     <div className="container mx-auto flex justify-center mt-16">
@@ -22,13 +22,15 @@ const Assignment = () => {
 
         <PieChart width={400} height={400}>
           <Pie
-            dataKey="marks"
-            startAngle={0}
-            endAngle={360}
             data={assignmentMarks}
-            outerRadius={100}
-            fill="#8884d8"
-          />
+            dataKey="marks"
+            nameKey="assignment"
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            label
+          ></Pie>
+          <Legend />
           <Tooltip />
         </PieChart>
       </div>
