83bfd7a0f5c1b8a8a149b523d7ec1af422ba1b93
nadim <mdnadimmahamud704@gmail.com>
2023-04-12 02:15:30 +0600

    job filter done

--- src/Component/AppliedJob/AppliedJob.jsx
+++ src/Component/AppliedJob/AppliedJob.jsx
@@ -6,10 +6,11 @@ import SingleJob from '../SingleJob/SingleJob';
 const AppliedJob = () => {
   const jobs = useLoaderData();
   const [job, setJob] = useState([]);
+  const [filteredJob, setFilteredJob] = useState([]);
 
   useEffect(() => {
     const savedCart = getShoppingCart();
-    let newArray = [];
+    const newArray = [];
     for (const id in savedCart) {
       const findJob = jobs.find((job) => job.id == id);
       if (findJob) {
@@ -17,21 +18,48 @@ const AppliedJob = () => {
       }
     }
     setJob(newArray);
+    setFilteredJob(newArray);
   }, []);
+
+  const filterOnSite = () => {
+    const filteredData = job.filter((fd) => fd.remoteOrOnsite === 'Onsite');
+    setFilteredJob(filteredData);
+  };
+
+  const filterOnRemote = () => {
+    const filteredData = job.filter((fd) => fd.remoteOrOnsite === 'Remote');
+    setFilteredJob(filteredData);
+  };
+
+  const resetFilter = () => {
+    setFilteredJob(job);
+  };
 
   return (
     <div>
       <h1 className="text-center mt-12 text-4xl font-bold">Applied Jobs</h1>
       <div className=" mt-32 mb-7">
         <div className="text-right container mx-auto mb-10">
-          <button className="px-5 py-3 bg-violet-500 text-lg font-semibold rounded-lg text-white">
+          <button
+            onClick={filterOnSite}
+            className="px-5 py-3 bg-violet-500 text-lg font-semibold rounded-lg text-white"
+          >
             onSite
           </button>
-          <button className="px-5 py-3 bg-violet-500 text-lg font-semibold rounded-lg text-white ml-10">
+          <button
+            onClick={filterOnRemote}
+            className="px-5 py-3 bg-violet-500 text-lg font-semibold rounded-lg text-white ml-10"
+          >
             Remote
           </button>
+          <button
+            onClick={resetFilter}
+            className="px-5 py-3 bg-violet-500 text-lg font-semibold rounded-lg text-white ml-10"
+          >
+            Show All
+          </button>
         </div>
-        {job.map((singleJob) => (
+        {filteredJob.map((singleJob) => (
           <SingleJob key={singleJob.id} singleJob={singleJob}></SingleJob>
         ))}
       </div>
